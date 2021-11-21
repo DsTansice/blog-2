7769af7cfcbc0f7ccefb93aca364ef41ab4f8ede
EmptyDreams <minedreams@qq.com>
2021-11-21 16:03:24 +0800

    .

--- gulpfile.js
+++ gulpfile.js
@@ -59,21 +59,5 @@ gulp.task('minify-html', () => {
         }))
         .pipe(gulp.dest('./public'))
 });
-//压缩图片
-gulp.task("compressImage", function () {
-    var option = {
-        optimizationLevel: 4, //类型：Number 默认：3 取值范围：0-7（优化等级）
-        progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
-        interlaced: false, //类型：Boolean 默认：false 隔行扫描gif进行渲染
-        multipass: false //类型：Boolean 默认：false 多次优化svg直到完全优化
-    };
-    return gulp
-        .src("./public/medias/**/*.*")
-        .pipe(gulpif(!isScriptAll, changed("./public/medias")))
-        .pipe(gulpif(isDebug, debug({ title: "Compress Images:" })))
-        .pipe(plumber())
-        .pipe(imagemin(option))
-        .pipe(gulp.dest("./public"));
-});
 
-gulp.task("default", gulp.series("generate-service-worker"), gulp.parallel('compress', 'minify-css', 'minify-html', 'compressImage'));
+gulp.task("default", gulp.series("generate-service-worker"), gulp.parallel('compress', 'minify-css', 'minify-html'));
