5a17a9ec9bcd7513c9ade814a945c0f7f8ffb760
EmptyDreams <minedreams@qq.com>
2022-02-18 17:22:28 +0800

    刷新缓存添加确认提示窗

--- themes/butterfly/source/js/custom/share.js
+++ themes/butterfly/source/js/custom/share.js
@@ -900,7 +900,7 @@ clipboard.on('error', function () {
 });
 function refreshCache() {
     if ('serviceWorker' in window.navigator && navigator.serviceWorker.controller) {
-        navigator.serviceWorker.controller.postMessage("refresh")
+        if (confirm('是否确定刷新博文缓存')) navigator.serviceWorker.controller.postMessage("refresh")
     } else {
         if (confirm('ServiceWorker未激活，受否刷新以激活SW')) location.reload()
     }
