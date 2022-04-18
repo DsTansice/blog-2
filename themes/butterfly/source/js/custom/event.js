6c298e7858d7040a1293f0f6c1c4e15b8dd37bfa
EmptyDreams <minedreams@qq.com>
2022-04-18 19:12:03 +0800

    添加公告

--- themes/butterfly/source/js/custom/event.js
+++ themes/butterfly/source/js/custom/event.js
@@ -22,10 +22,12 @@ navigator.serviceWorker.addEventListener('message', event => {
         case 'success':
             location.reload()
             break
-        case 'location':
-            if (GLOBAL_CONFIG.Snackbar) {
-                btf.snackbarShow('检测到您的弱网络环境……<br/><br/>在后台加载完毕后，刷新页面即可查看新的内容')
-            }
-            break
     }
 })
+
+let cout = localStorage.getItem('amount');
+if (!cout) cout = 0
+if (cout < 1111) {
+    localStorage.setItem('amount', cout + 1)
+    btf.snackbarShow('<p>如果您访问本站时出现大批量图片无法加载的情况<p>还请您即时反馈给我，谢谢合作~')
+}
