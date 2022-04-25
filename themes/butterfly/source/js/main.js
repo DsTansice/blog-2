b5d96a23f225cd34d631aee3b3462f6638d4f6ff
EmptyDreams <minedreams@qq.com>
2022-04-25 22:56:43 +0800

    追番页面屏蔽灯箱

--- themes/butterfly/source/js/main.js
+++ themes/butterfly/source/js/main.js
@@ -222,6 +222,7 @@ document.addEventListener('DOMContentLoaded', function () {
    * Lightbox
    */
   const runLightbox = () => {
+    if (location.href.match('/bangumis/')) return
     btf.loadLightbox(document.querySelectorAll('#article-container img:not(.no-lightbox):not(.inline)'))
   }
 
