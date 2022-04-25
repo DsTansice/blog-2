f72141d05871082737057e0310ebac684e2bc6a2
EmptyDreams <minedreams@qq.com>
2022-04-25 22:33:12 +0800

    追番页面点击按钮自动回到头部

---
+++ themes/butterfly/source/js/custom/bangumis.js
@@ -0,0 +1,23 @@
+/**
+ * 获取元素纵坐标
+ * @param element 元素对象
+ * @returns {number}
+ */
+function getElementTop(element) {
+    let actualTop = element.offsetTop;
+    let current = element.offsetParent;
+    while (current !== null) {
+        actualTop += current.offsetTop;
+        current = current.offsetParent;
+    }
+    return actualTop;
+}
+
+//追番页面点击按钮时回到顶部
+window.addEventListener('load', () => {
+    const index = getElementTop(document.getElementById('page'))
+    const list = document.getElementsByClassName('bangumi-button')
+    for (let element of list) {
+        element.addEventListener('click', () => btf.scrollToDest(index))
+    }
+})
