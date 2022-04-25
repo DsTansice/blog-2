ab9dae25b1c3258956cbd3ed28d5e5220e6011a6
EmptyDreams <minedreams@qq.com>
2022-04-25 20:32:14 +0800

    移除开屏公告

--- themes/butterfly/source/js/custom/event.js
+++ themes/butterfly/source/js/custom/event.js
@@ -24,10 +24,3 @@ navigator.serviceWorker.addEventListener('message', event => {
             break
     }
 })
-
-let cout = localStorage.getItem('amount');
-if (!cout) cout = 0
-if (cout < 1111) {
-    localStorage.setItem('amount', cout + 1)
-    btf.snackbarShow('<p>如果您访问本站时出现大批量图片无法加载的情况<p>还请您即时反馈给我，谢谢合作~')
-}
