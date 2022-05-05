0c69007456091bb4528eea5adf2a7dfd8ae869f1
EmptyDreams <minedreams@qq.com>
2022-05-05 19:47:35 +0800

    gulp压缩字体

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
-const gulp = require("gulp");
+const gulp = require("gulp")
 //用到的各个插件
-var cleanCSS = require('gulp-clean-css');
-var htmlmin = require('gulp-html-minifier-terser');
-var htmlclean = require('gulp-htmlclean');
-const replace = require('gulp-replace');
-// gulp-tester
-var terser = require('gulp-terser');
+const cleanCSS = require('gulp-clean-css')
+const htmlMin = require('gulp-html-minifier-terser')
+const htmlClean = require('gulp-htmlclean')
+const replace = require('gulp-replace')
+const terser = require('gulp-terser')
+const ttf2woff2 = require('gulp-ttf2woff2')
+const fontMin = require('gulp-fontmin');
 
 // 压缩js
 gulp.task('compress', () =>
-  gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
-    .pipe(terser())
-    .pipe(gulp.dest('./public'))
+    gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
+        .pipe(terser())
+        .pipe(gulp.dest('./public'))
 )
 //压缩css
 gulp.task('minify-css', () => {
@@ -24,8 +25,8 @@ gulp.task('minify-css', () => {
 //压缩html
 gulp.task('minify-html', () => {
     return gulp.src('./public/**/*.html')
-        .pipe(htmlclean())
-        .pipe(htmlmin({
+        .pipe(htmlClean())
+        .pipe(htmlMin({
             removeComments: true, //清除html注释
             collapseWhitespace: true, //压缩html
             collapseBooleanAttributes: true,
@@ -42,13 +43,45 @@ gulp.task('minify-html', () => {
         }))
         .pipe(gulp.dest('./public'))
 });
+
+//压缩字体
+function minifyFont(text, cb) {
+    gulp
+        .src('./public/font/*.ttf') //原字体所在目录
+        .pipe(fontMin({text: text}))
+        .pipe(gulp.dest('./public/font/')) //压缩后的输出目录
+        .on('end', cb);
+}
+
 //替换CDN
-gulp.task('cdn', async() => {
-    gulp.src('public/**/*.*')
-        .pipe(replace('unpkg.zhimg.com', 'npm.elemecdn.com'))
-        .pipe(replace('cdn.jsdelivr.net/npm', 'npm.elemecdn.com'))
-        .pipe(replace('cdn.jsdelivr.net/gh', 'cdn1.tianli0.top/gh'))
-        .pipe(gulp.dest('public/')), { overwrite: true };
+gulp.task('cdn', async () => {
+    gulp.src('./public/**/*.*')
+        .pipe(replace('https://unpkg.zhimg.com', 'https://npm.elemecdn.com'))
+        .pipe(replace('https://cdn.jsdelivr.net/npm', 'https://npm.elemecdn.com'))
+        .pipe(replace('https://cdn.jsdelivr.net/gh', 'https://cdn1.tianli0.top/gh'))
+        .pipe(gulp.dest('./public/')), {overwrite: true};
 });
 
+//转换字体
+gulp.task('ttf2woff2', async () => {
+    gulp.src(['./public/font/*.ttf'])
+        .pipe(ttf2woff2())
+        .pipe(gulp.dest('./public/font/'));
+})
+
+//压缩字体
+gulp.task('mini-font', (cb) => {
+    const buffers = [];
+    gulp
+        .src(['./public/**/*.html']) //HTML文件所在目录请根据自身情况修改
+        .on('data', function(file) {
+            buffers.push(file.contents);
+        })
+        .on('end', function() {
+            const text = Buffer.concat(buffers).toString('utf-8');
+            minifyFont(text, cb);
+        });
+});
+
+//压缩
 gulp.task("zip", gulp.parallel('compress', 'minify-css', 'minify-html'));
