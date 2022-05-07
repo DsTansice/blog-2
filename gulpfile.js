1a7b96ab4c5231e8b86e863a7a695aacd58c2850
EmptyDreams <minedreams@qq.com>
2022-05-08 00:04:44 +0800

    不再压缩字体

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,7 @@ const htmlMin = require('gulp-html-minifier-terser')
 const htmlClean = require('gulp-htmlclean')
 const replace = require('gulp-replace')
 const terser = require('gulp-terser')
-const ttf2woff2 = require('gulp-ttf2woff2')
-const fontMin = require('gulp-fontmin')
-const jsonMin = require('gulp-jsonmin');
+const jsonMin = require('gulp-jsonmin')
 
 // 压缩js
 gulp.task('compress', () =>
@@ -50,14 +48,6 @@ gulp.task('min-json', async () => {
         .pipe(gulp.dest('./dist'));
 });
 
-//压缩字体
-function minifyFont(text, cb) {
-    gulp.src('./public/font/s*.ttf') //原字体所在目录
-        .pipe(fontMin({text: text}))
-        .pipe(gulp.dest('./public/font/')) //压缩后的输出目录
-        .on('end', cb)
-}
-
 //替换CDN
 gulp.task('cdn', async () => {
     gulp.src('./public/**/*.*')
@@ -67,25 +57,7 @@ gulp.task('cdn', async () => {
         .pipe(gulp.dest('./public/')), {overwrite: true}
 })
 
-//转换字体
-gulp.task('ttf2woff2', async () => {
-    gulp.src(['./public/font/*.ttf'])
-        .pipe(ttf2woff2())
-        .pipe(gulp.dest('./public/font/'))
-})
-
-//压缩字体
-gulp.task('mini-font', (cb) => {
-    const buffers = [];
-    gulp.src(['./public/**/*.html'])
-        .on('data', (file) => buffers.push(file.contents))
-        .on('end', () => {
-            const text = Buffer.concat(buffers).toString('utf-8')
-            minifyFont(text, cb)
-        })
-})
-
 //压缩
-gulp.task("zip", gulp.parallel('compress', 'minify-css', 'minify-html', 'mini-font', 'min-json'))
+gulp.task("zip", gulp.parallel('compress', 'minify-css', 'minify-html', 'min-json'))
 //转换
 gulp.task("convert", gulp.parallel('cdn'))
